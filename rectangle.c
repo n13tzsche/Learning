ca622d2b428ca8934ab5922c8b74223be1a24ae2
n13tzsche <tlicznerski@gmail.com>
2020-11-10 22:10:50 +0000

    thanks to this you can calculate perimeter and field of rectangle v1

---
+++ rectangle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void)
+{
+	int x, y;
+
+	printf("Podaj dlugosc boku x\n");
+	scanf("%d", &x);
+
+	printf("Podaj dlugosc boku y\n");
+	scanf("%d", &y);
+
+	printf("Pole prostokata = %d\n", x * y);
+	printf("Obwod prostokata - %d\n", (2*x) + (2*y));
+		
+
+       return 0;
+}
+
