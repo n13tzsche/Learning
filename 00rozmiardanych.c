02942b5feb35d33bee985b3e5a2aa7cacf5b86f1
n13tzsche <tlicznerski@gmail.com>
2020-11-17 22:19:01 +0000

    rozmiar poszczegolnych danych w bitach

---
+++ 00rozmiardanych.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf("size of int : %ld\n", sizeof(int));
+	printf("size of signed int : %ld\n", sizeof(signed int));
+	printf("size of unsigned long : %ld\n", sizeof(unsigned long));
+	printf("size of long long : %ld\n", sizeof(long long));
+	printf("size of doule : %ld\n", sizeof(double));
+	printf("size of float : %ld\n", sizeof(float));
+	return 0;
+}
